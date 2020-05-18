bde1e7ac26abf41ec9b771fd10c4a28fe528509e
Nick Williams <NickWilliams@benefex.co.uk>
2020-05-18 15:28:28 +0100

    Updated step definitions

--- src/main/java/stepdefinitions/Stepdefs.java
+++ src/main/java/stepdefinitions/Stepdefs.java
@@ -4,22 +4,31 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return null;
+    }
+}
+
+
 public class Stepdefs {
+    private String today;
+    private String actualAnswer;
 
     @Given("today is Sunday")
     public void today_is_Sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("Yep this is working");
+        today = "Sunday";
     }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_Friday_yet() {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("working");
+        actualAnswer = IsItFriday.isItFriday(today);
+        //System.out.println("working");
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
-    public void shouldBeTold(String arg0){
-        System.out.println("test");
+    public void I_should_be_bold(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer)
     }
 }
