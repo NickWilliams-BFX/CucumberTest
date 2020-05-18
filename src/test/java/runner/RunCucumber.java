9b745ab6f2cba83b36c9e2589753d0f5bfc98389
Nick Williams <NickWilliams@benefex.co.uk>
2020-05-18 13:14:03 +0100

    First Commit of Cucumber Test Tutorial

---
+++ src/test/java/runner/RunCucumber.java
@@ -0,0 +1,14 @@
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"json:target/reports/cucumber.json", "json:target/reports/cucumber.cucumber",
+        "pretty"}, features = {"src/test/resources/hellocucumber"}, glue = {"Stepdefs"})
+
+
+public class RunCucumber {
+}
